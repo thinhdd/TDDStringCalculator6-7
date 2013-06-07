59612025d656291c04873d2fb60b77288efc948d
thinhdd <thinhdd_54@vnu.edu.vn>
2013-06-07 23:49:11 +0700

    3 green

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -9,6 +9,21 @@ public class StringCalculator {
     public static int add(String s) {
         if(s.isEmpty())
             return 0;
+        if (s.contains(","))
+        {
+            return Sum(s, ",");
+        }
         return Integer.parseInt(s);  //To change body of created methods use File | Settings | File Templates.
+    }
+    public static int Sum(String s, String deli)
+    {
+        int sum=0;
+        String cur[] = s.split(deli);
+        for (String i: cur)
+        {
+            if(!i.isEmpty())
+                sum=sum+Integer.parseInt(i);
+        }
+        return sum;
     }
 }
